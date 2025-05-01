1dc5cc9aca880eddcdcd0ddbdf90cce247e86c03
Josh Wild <jwild2099@gmail.com>
2025-04-30 21:03:54 -0500

    Part 2 Completed- Setting up game board with pellet nodes.

---
+++ Assets/Scripts/SpawnNodes.cs
@@ -0,0 +1,25 @@
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNodes : MonoBehaviour
+{
+        int numToSpawn = 28;
+        public float spawnOffset = 0.3f;
+        public float currentSpawnOffset;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+                gameObject.name = "Node";
+                return;
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+}
